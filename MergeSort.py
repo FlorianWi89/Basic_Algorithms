f29048ab6e10ed8a30d20e27757f1706a0dd139d
Florianwi98 <florianwicher94@gmail.com>
2021-09-23 14:39:51 +0200

    MergeSort in Python

---
+++ MergeSort.py
@@ -0,0 +1,30 @@
+
+def mergeSort(array):
+    if len(array) > 1:
+        middle = len(array)//2
+        L = array[:middle]
+        R = array[middle:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if(L[i] < R[j]):
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = R[j]
+                j += 1
+            k += 1
+
+            while i < len(L):
+                array[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                array[k] = R[j]
+                j += 1
+                k += 1
